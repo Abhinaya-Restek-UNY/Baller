1593b44939417a1e6d9fad1ee26eb5ddc77d7101
Febri Bayu Nurcahyo <febribayun@gmail.com>
2026-02-08 08:18:47 +0700

    add: arduino style hooks.

---
+++ stm32/src/main.cpp
@@ -0,0 +1,15 @@
+#include "hooks.hpp"
+
+void beginning_Hook() {};
+
+void HAL_Init_Hook() {};
+
+void SysInit_Hook() {};
+
+void setup() {
+  // placeholder
+  while (true) {
+  };
+};
+
+void loop() {};
